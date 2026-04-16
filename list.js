3c505c1a45548242a70edfa13cc31dccb03a549d
todoom1 <aa13976383@outlook.com>
2026-04-17 05:20:40 +0800

    Add files via upload

--- list.js
+++ list.js
@@ -1,65 +1,51 @@
-window["document"]['getElementById']("bsj")['addEventListener']("click", function () {
-    window['open'](atob("aHR0cHM6Ly93eC5ndGx5c2MuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMzQ1MjMyNzUmdG9wSWQ9MTYyMDA2Ng"), '_blank')
-});
-window["document"]['getElementById']("st")['addEventListener']("click", function () {
-    window['open'](atob("aHR0cHM6Ly9sZWp2YS50cHJzbWkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDU4JnRvcElkPTIwNzI5ODgmc2VsZlBsYW5JZD0xNjY1ODA3"), '_blank')
-});
-window["document"]['getElementById']("wd")['addEventListener']("click", function () {
-    window['open'](atob("aHR0cHM6Ly9vbmtyY3kuZm5icHN3LmNvbS9hcHAvcmVnaXN0ZXIucGhwP3NpdGVfaWQ9MTAyMCZ0b3BJZD0zNzAzNjk3Mg"), '_blank')
-});
-window["document"]['getElementById']("ff")['addEventListener']("click", function () {
-    window['open'](atob("aHR0cHM6Ly9sbmNxYy5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDUwJnRvcElkPTQwMTE3NzE3NjY2ODkxNjImc2VsZlBsYW5JZD00MjY2MjQ5"), '_blank')
-});
-window["document"]['getElementById']("gjr")['addEventListener']("click", function () {
-    window['open'](atob("aHR0cHM6Ly90ei53eGdqcjUuY2MvYXBwL3JlZ2lzdGVyLnBocD9zaXRlX2lkPTIzMDQmdG9wSWQ9MTc5Njk0OQ"), '_blank')
-});
-window["document"]['getElementById']("jl")['addEventListener']("click", function () {
-    window['open'](atob("aHR0cHM6Ly90ZmFqbi5oY2p3ai5jb20vYXBwL3JlZ2lzdGVyLnBocD9zaXRlX2lkPTIwODEmdG9wSWQ9MTY4MTQzMCZzZWxmUGxhbklkPTE1NTkwMTg"), '_blank')
-});
-window["document"]['getElementById']("sj")['addEventListener']("click", function () {
-    window['open'](atob("aHR0cHM6Ly94anZ1ZWIuZm5icHN3LmNvbS9hcHAvcmVnaXN0ZXIucGhwP3NpdGVfaWQ9MTAxMCZ0b3BJZD04NTIxNzY0MzEyODgyMzE3"), '_blank')
-});
-window["document"]['getElementById']("xsd")['addEventListener']("click", function () {
-    window['open'](atob("aHR0cHM6Ly9kZXJlYXIuZm5icHN3LmNvbS9hcHAvcmVnaXN0ZXIucGhwP3NpdGVfaWQ9MTA1OSZ0b3BJZD0xNTg5MzI2JnNlbGZQbGFuSWQ9MTI3NTAyNA"), '_blank')
-});
-window["document"]['getElementById']("yh")['addEventListener']("click", function () {
-    window['open'](atob("aHR0cHM6Ly9nZnZneC5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD04MDAmcHQ9NUJFMkI0QkQtNDE2OS1DNkRGLTZDQTMtMDE5OEM1QTA4NDQx"), '_blank')
-});
-window["document"]['getElementById']("dt")['addEventListener']("click", function () {
-    window['open'](atob("aHR0cHM6Ly90ZmFqbi5oY2p3ai5jb20vYXBwL3JlZ2lzdGVyLnBocD9zaXRlX2lkPTEwNjMmcHQ9QjgwREVBRUQtQjVCRC00QTc4LTY4QUYtODQ0QkEzMTEyOTY1"), '_blank')
-});
-window["document"]['getElementById']("ng")['addEventListener']("click", function () {
-    window['open'](atob("aHR0cHM6Ly8xNDkuMzAuMTYzLjIzNTozMjAxMS8jL2xpbms/YWxsd2luPVlMZk9RYmtabzUzN0tPbWQzbTQxSWclM0QlM0Q"), '_blank')
-});
-window["document"]['getElementById']("df")['addEventListener']("click", function () {
-    window['open'](atob("aHR0cHM6Ly9nZnZneC5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDMwJnRvcElkPTEzODA2DQ=="), '_blank')
-});
-window["document"]['getElementById']("cf")['addEventListener']("click", function () {
-    window['open'](atob("aHR0cHM6Ly9wa2FqaW0uaGZqY2wuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDMyJnRvcElkPTIyOTg0DQ=="), '_blank')
-});
-window["document"]['getElementById']("t1")['addEventListener']("click", function () {
-    window['open'](atob("aHR0cHM6Ly9sbmNxYy5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDUxJnRvcElkPTUyNzQxOQ0="), '_blank')
-});
-window["document"]['getElementById']("c7")['addEventListener']("click", function () {
-    window['open'](atob("aHR0cHM6Ly8zOC4xODEuMjMuMjU6NjAwMDUvIy9saW5rP2FsbHdpbj03UWpVRVhYa3NXd2E2ZGE2SlFFJTJCc2clM0QlM0Q="), '_blank')
-});
-window["document"]['getElementById']("yhn")['innerHTML'] = ("壹号娱乐");
-window["document"]['getElementById']("dtn")['innerHTML'] = ("鼎天国际");
-window["document"]['getElementById']("ngn")['innerHTML'] = ("NG28");
-window["document"]['getElementById']("t1n")['innerHTML'] = ("TOP1体育");
-window["document"]['getElementById']("c7n")['innerHTML'] = ("C7");
-window["document"]['getElementById']("stn")['innerHTML'] = ("胜天国际");
-window["document"]['getElementById']("cfn")['innerHTML'] = ("超凡国际");
-window["document"]['getElementById']("wdn")['innerHTML'] = ("问鼎娱乐");
-window["document"]['getElementById']("ffn")['innerHTML'] = ("非凡国际");
-window["document"]['getElementById']("gjrn")['innerHTML'] = ("国际人");
-window["document"]['getElementById']("jln")['innerHTML'] = ("君临国际");
-window["document"]['getElementById']("sjn")['innerHTML'] = ("赏金国际");
-window["document"]['getElementById']("xsdn")['innerHTML'] = ("新时代");
-window["document"]['getElementById']("dfn")['innerHTML'] = ("巅峰国际");
-window["document"]['getElementById']("bsjn")['innerHTML'] = ("保时捷");
+(function() {
+    var secretApps = [
+{img: './png/bsj.jpg', name: '保时捷', xurl: 'aHR0cHM6Ly93eC5ndGx5c2MuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMzQ1MjMyNzUmdG9wSWQ9MTYyMDA2Ng'},
+{img: './png/yh.png', name: '壹号娱乐', xurl: 'aHR0cHM6Ly9nZnZneC5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD04MDAmcHQ9NUJFMkI0QkQtNDE2OS1DNkRGLTZDQTMtMDE5OEM1QTA4NDQx'},
+{img: './png/ffyl.jpg', name: '非凡国际', xurl: 'aHR0cHM6Ly9sbmNxYy5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDUwJnRvcElkPTQwMTE3NzE3NjY2ODkxNjImc2VsZlBsYW5JZD00MjY2MjQ5'},
+{img: './png/dtgj.jpg', name: '鼎天国际', xurl: 'aHR0cHM6Ly90ZmFqbi5oY2p3ai5jb20vYXBwL3JlZ2lzdGVyLnBocD9zaXRlX2lkPTEwNjMmcHQ9QjgwREVBRUQtQjVCRC00QTc4LTY4QUYtODQ0QkEzMTEyOTY1'},
+{img: './png/ng.jpg', name: 'NG28', xurl: 'aHR0cHM6Ly8xNDkuMzAuMTYzLjIzNTozMjAxMS8jL2xpbms/YWxsd2luPVlMZk9RYmtabzUzN0tPbWQzbTQxSWclM0QlM0Q'},
+{img: './png/stgj.jpg', name: '胜天国际', xurl: 'aHR0cHM6Ly9sZWp2YS50cHJzbWkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDU4JnRvcElkPTIwNzI5ODgmc2VsZlBsYW5JZD0xNjY1ODA3'},
+{img: './svg/wd.svg', name: '问鼎娱乐', xurl: 'aHR0cHM6Ly9vbmtyY3kuZm5icHN3LmNvbS9hcHAvcmVnaXN0ZXIucGhwP3NpdGVfaWQ9MTAyMCZ0b3BJZD0zNzAzNjk3Mg'},
+{img: './png/gjr.jpg', name: '国际人', xurl: 'aHR0cHM6Ly90ei53eGdqcjUuY2MvYXBwL3JlZ2lzdGVyLnBocD9zaXRlX2lkPTIzMDQmdG9wSWQ9MTc5Njk0OQ'},
+{img: './png/jlgj.jpg', name: '君临国际', xurl: 'aHR0cHM6Ly90ZmFqbi5oY2p3ai5jb20vYXBwL3JlZ2lzdGVyLnBocD9zaXRlX2lkPTIwODEmdG9wSWQ9MTY4MTQzMCZzZWxmUGxhbklkPTE1NTkwMTg'},
+{img: './png/sjgj.png', name: '赏金国际', xurl: 'aHR0cHM6Ly94anZ1ZWIuZm5icHN3LmNvbS9hcHAvcmVnaXN0ZXIucGhwP3NpdGVfaWQ9MTAxMCZ0b3BJZD04NTIxNzY0MzEyODgyMzE3'},
+{img: './png/xsd.jpg', name: '新时代', xurl: 'aHR0cHM6Ly9kZXJlYXIuZm5icHN3LmNvbS9hcHAvcmVnaXN0ZXIucGhwP3NpdGVfaWQ9MTA1OSZ0b3BJZD0xNTg5MzI2JnNlbGZQbGFuSWQ9MTI3NTAyNA'},
+{img: './png/dfgj.jpg', name: '巅峰国际', xurl: 'aHR0cHM6Ly9nZnZneC5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDMwJnRvcElkPTEzODA2DQ=='},
+{img: './png/top1.png', name: 'TOP1体育', xurl: 'aHR0cHM6Ly9sbmNxYy5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDUxJnRvcElkPTUyNzQxOQ0='},
+{img: './png/c7.png', name: 'C7', xurl: 'aHR0cHM6Ly8zOC4xODEuMjMuMjU6NjAwMDUvIy9saW5rP2FsbHdpbj03UWpVRVhYa3NXd2E2ZGE2SlFFJTJCc2clM0QlM0Q='},
+{img: './png/cfgj.jpg', name: '超凡国际', xurl: 'aHR0cHM6Ly9wa2FqaW0uaGZqY2wuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDMyJnRvcElkPTIyOTg0DQ=='}
+    ];
 
-window["document"]['getElementById']("3")['innerHTML'] = ("世界杯World Cup 2026™<br>官方合作广告商");
+    var container = document.getElementById('xapes');
+    if (!container) return;
+
+    secretApps.forEach(function(app) {
+        // 1. 创建元素
+        var li = document.createElement('li');
+        li.className = 'app-item'; // 只保留类名用于样式
+
+        li.innerHTML = `
+            <div class="app-icon"><img src="${app.img}"></div>
+            <div class="app-name">${app.name}</div>
+        `;
+
+        li.onclick = function() {
+            try {
+                // 解码并跳转
+                var targetUrl = atob(app.xurl.trim());
+                window.open(targetUrl, '_blank');
+            } catch (e) {
+                console.error("解码失败:", e);
+            }
+        };
+
+        // 4. 塞进容器
+        container.appendChild(li);
+    });
+})();
+
+window["document"]['getElementById']("3")['innerHTML'] = ("2026世界杯<br>点击查看赛程");
 window["document"]['getElementById']("2")['innerHTML'] = ("🔥十年信誉平台<br>⚽世界杯官方投注平台");
 window["document"]['getElementById']("1")['innerHTML'] = "11PN.MY";
 
